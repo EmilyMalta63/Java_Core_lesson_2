4ce049c8a83dc54dc009df7b2ba366ca49bf29c5
EmilyMalta63 <emilymalta@ukr.net>
2022-11-01 13:54:07 +0200

    added first project to github

---
+++ less2/src/less2/aplication.java
@@ -0,0 +1,18 @@
+package less2;
+
+public class aplication {
+public static void main(String[] args) {
+	Byte a1=2;
+	Short a2=2;
+	Integer a3= 2;
+	Long a4 = 2L;
+	
+	int a5=a1;
+	a3=a5;
+	
+	Double b= 52.45;
+	Float b1 = 4.4F;
+	System.out.println("Byte = "+ Byte.MAX_VALUE);
+	System.out.println("Byte = "+ Byte.MIN_VALUE);
+}
+}
